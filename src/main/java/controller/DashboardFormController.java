3aeef9192cb423cd9bbb60160e23a8cb8e06be7e
AmilSrinath <amilsrinath5@gmail.com>
2024-03-04 22:33:28 +0530

    set Manage Animal button in dashboard

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -4,19 +4,27 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class DashboardFormController {
+    public Button btnManageAnimal;
+
+    boolean formOpened = false;
     @FXML
     void manageAnimalBtnOnAction(ActionEvent event) throws IOException {
-        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/animalForm.fxml"));
-        Stage stage = new Stage();
-        stage.setScene(new Scene(anchorPane));
-        stage.show();
-        stage.centerOnScreen();
-        stage.setTitle("Wildlife Management System - Animal Page");
+        if (!formOpened) {
+            AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/animalForm.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(anchorPane));
+            stage.show();
+            stage.centerOnScreen();
+            stage.setTitle("Wildlife Management System - Animal Page");
+            formOpened = true;
+            btnManageAnimal.setDisable(true);
+        }
     }
 }
