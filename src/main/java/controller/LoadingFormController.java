551134a9d1a96364928e732d8bfedf995fedcfe3
AmilSrinath <amilsrinath5@gmail.com>
2024-03-04 09:03:41 +0530

    Second Commit

--- src/main/java/controller/LoadingFormController.java
+++ src/main/java/controller/LoadingFormController.java
@@ -20,38 +20,38 @@ public class LoadingFormController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        Task<Void> task = new Task<Void>() {
-//            @Override
-//            protected Void call() throws Exception {
-//                for (int i = 0; i <= 100; i++) {
-//                    updateProgress(i,55);
-//                    Thread.sleep(55);
-//                }
-//                return null;
-//            }
-//        };
-//
-//        progressgBar.progressProperty().bind(task.progressProperty());
-//        task.setOnSucceeded(event -> {
-//
-//            Parent loginParent = null;
-//            try {
-//                loginParent = FXMLLoader.load(getClass().getResource("/view/dashboardForm.fxml"));
-//                Scene loginScene = new Scene(loginParent);
-//                Stage loginStage = new Stage();
-//                loginStage.setResizable(false);
-//                Image image = new Image(getClass().getResourceAsStream("/image/icons8-bird-96 (1).png"));
-//                loginStage.getIcons().add(image);
-//                loginStage.setTitle("Juice Bar Management System - Login Page");
-//                loginStage.setScene(loginScene);
-//                loginStage.show();
-//                ((Stage)progressgBar.getScene().getWindow()).close();
-//
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//
-//        });
-//        new Thread(task).start();
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                for (int i = 0; i <= 100; i++) {
+                    updateProgress(i,55);
+                    Thread.sleep(55);
+                }
+                return null;
+            }
+        };
+
+        progressgBar.progressProperty().bind(task.progressProperty());
+        task.setOnSucceeded(event -> {
+
+            Parent loginParent = null;
+            try {
+                loginParent = FXMLLoader.load(getClass().getResource("/view/dashboardForm.fxml"));
+                Scene loginScene = new Scene(loginParent);
+                Stage loginStage = new Stage();
+                loginStage.setResizable(false);
+                Image image = new Image(getClass().getResourceAsStream("/image/icons8-bird-96 (1).png"));
+                loginStage.getIcons().add(image);
+                loginStage.setTitle("Juice Bar Management System - Login Page");
+                loginStage.setScene(loginScene);
+                loginStage.show();
+                ((Stage)progressgBar.getScene().getWindow()).close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+        new Thread(task).start();
     }
 }
