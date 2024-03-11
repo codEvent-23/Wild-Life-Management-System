d087de0580a78fd6dbd23a0a25f643d22b1d5a5e
AmilSrinath <amilsrinath5@gmail.com>
2024-03-11 22:26:49 +0530

    set Label for Dashboard

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -4,12 +4,20 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,6 +30,14 @@ public class DashboardFormController implements Initializable {
     public ImageView imgDashboardLogo;
     public Button btnLogout;
     public AnchorPane dashboardForm;
+    public Label lblDetails;
+    public Label lblAnimalName;
+    public AnchorPane detailsPane;
+    public HBox mapHBox;
+    public HBox CommonLocationHBox;
+    public HBox imgHBox;
+    public VBox mainVBox;
+    public ScrollPane mainScrollPane;
     // Reference to the AnchorPane defined in the FXML file
     @FXML
     private AnchorPane animalDetailsAnchorPane;
@@ -70,8 +86,36 @@ public class DashboardFormController implements Initializable {
     // Method called when the "Upload Image" button is clicked
     @FXML
     void uploadImgBtnOnAction(ActionEvent event) {
-        // Make the animalDetailsAnchorPane visible
         animalDetailsAnchorPane.setVisible(true);
+        mainVBox.getChildren().clear();
+
+        Label tital = new Label("(Animal Name)");
+        tital.setFont(new Font("Poppins", 20));
+
+        Label detail = new Label("\n"+"The Wildlife Management System employs JavaFX and MongoDB, leveraging " +
+                "Object-Oriented Programming for seamless information management of diverse wildlife species. With " +
+                "a user-friendly interface, it offers a loading page, a dynamic dashboard for searching and managing " +
+                "animals, detailed information retrieval, and image recognition capabilities.\n\nThe Wildlife Management " +
+                "System employs JavaFX and MongoDB, leveraging Object-Oriented Programming for seamless information " +
+                "management of diverse wildlife species. With a user-friendly interface, it offers a loading page, a " +
+                "dynamic dashboard for searching and managing animals, detailed information retrieval, and image " +
+                "recognition capabilities.\n\nThe Wildlife Management System employs JavaFX and MongoDB, leveraging " +
+                "Object-Oriented Programming for seamless information management of diverse wildlife species. With a " +
+                "user-friendly interface, it offers a loading page, a dynamic dashboard for searching and managing " +
+                "animals, detailed information retrieval, and image recognition capabilities.\n\nThe Wildlife Management " +
+                "System employs JavaFX and MongoDB, leveraging Object-Oriented Programming for seamless information " +
+                "management of diverse wildlife species. With a user-friendly interface, it offers a loading page, " +
+                "a dynamic dashboard for searching and managing animals, detailed information retrieval, and image " +
+                "recognition capabilities.");
+        detail.setWrapText(true);
+        detail.setTextAlignment(TextAlignment.JUSTIFY);
+        detail.setFont(new Font("Calibri", 16));
+
+        HBox hBox = new HBox();
+        mainVBox.setPadding(new Insets(20, 30, 20, 20));
+        hBox.getChildren().add(detail);
+
+        mainVBox.getChildren().addAll(imgHBox, CommonLocationHBox, mapHBox, tital, hBox);
     }
 
     // Method called during the initialization of the controller
