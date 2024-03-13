f6ee313bfb9121245895dc92d1fff29ae13efb4e
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-03-14 05:03:59 +0530

    add: animal entity

--- src/main/java/entity/Animal.java
+++ src/main/java/entity/Animal.java
@@ -1,4 +1,38 @@
 package entity;
 
+import dev.morphia.annotations.Entity;
+import dev.morphia.annotations.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+@Entity("animal")
 public class Animal {
+
+    @Id
+    private ObjectId id;
+    private String species;
+    private String common_name;
+    private String scientific_name;
+    private String gender;
+    private Integer average_life_time;
+    private Double average_weight;
+    private String region;
+    private String conservation_status;
+    private String reproduction;
+    private String color;
+    private String markings;
+    private String habitat;
+    private String behavior;
+    private String dietary_preferences;
+    private String location;
+    private String additional_details;
+    private List<byte[]> images;
+
 }
